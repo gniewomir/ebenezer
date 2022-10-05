1da85dd99f90780e7eff55b41ea709d312299229
Gniewomir Świechowski <gniewomir.swiechowski@gmail.com>
2022-10-05 18:49:28 +0200

    Scaffold namespaces for the project

--- src/Enraged/Ebenezer/Infrastructure/Symfony/Kernel.php
+++ src/Enraged/Ebenezer/Infrastructure/Symfony/Kernel.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace Enraged\Ebenezer\Infrastructure\Symfony;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
